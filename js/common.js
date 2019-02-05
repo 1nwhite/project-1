9e61769cc2e27f044df86786897d0b90ca311295
Sergey Grebtsov <inwhite96@gmail.com>
2019-02-05 12:10:08 +0200

    second commit

--- js/common.js
+++ js/common.js
@@ -27,5 +27,22 @@ $(document).ready(function() {
         $(this).toggleClass('header-btn_active');
         $('.header-menu').toggleClass('header-menu_active')
     });	
+
+//////////// Phone hover
+	var touchHover = function() {
+	    $('[data-hover]').click(function(e){
+	        e.preventDefault();
+	        var $this = $(this);
+	        var onHover = $this.attr('data-hover');
+	        var linkHref = $this.attr('href');
+	        if (linkHref && $this.hasClass(onHover)) {
+	            location.href = linkHref;
+	            return false;
+	        }
+	        $this.toggleClass(onHover);
+	    });
+	};
+
+	$(document).ready(function() { touchHover(); });
 		
 });
